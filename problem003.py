a403eceb7860b919bae38a89a714e03c17183269
Jaime Paz Lopes <61919395+JaimePazLopes@users.noreply.github.com>
2020-03-08 17:20:22 -0300

    Update problem003.py

--- problem003.py
+++ problem003.py
@@ -1,3 +1,4 @@
+# Problem #2 [Medium]
 # Given the root to a binary tree, implement serialize(root), which serializes the tree into a string,
 # and deserialize(s), which deserializes the string back into the tree.
 import pickle
