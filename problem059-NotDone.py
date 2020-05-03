8a9e9a244cfd774da1f6a9ed5c260ba9c9f48013
Jaime Paz Lopes <61919395+JaimePazLopes@users.noreply.github.com>
2020-05-03 15:23:48 -0300

    uploading just to have the file and right counting

---
+++ problem059-NotDone.py
@@ -0,0 +1,6 @@
+# Problem #59 [Hard]
+# Implement a file syncing algorithm for two computers over a low-bandwidth network.
+# What if we know the files in the two computers are mostly the same?
+#
+# honestly i dont even know where to start on this one, tried to google some stuff, but didnt found much.
+# And most of what i found have a high complexity. Maybe in the future i have some more knowledge to try this again.
